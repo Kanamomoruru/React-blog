a13d60f14c21923a74820347c193737b45a286da
Kanata <thunderpooh3@gmail.com>
2020-07-11 20:15:37 +0900

    Add backgroundImage

--- src/App.js
+++ src/App.js
@@ -3,11 +3,26 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 import './App.css';
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
     background: "#fff"
+  },
+  hero: {
+    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+    url('https://images.unsplash.com/photo-1542324253097-09d465f007bc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80')`,
+    height: "500px",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    position: "relative",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    color: "#fff",
+    fontSize: "4rem"
   }
 }));
 
@@ -22,6 +37,9 @@ function App() {
           </Typography>
         </Toolbar>
       </AppBar>
+      <Box className={classes.hero}>
+        <Box>React Blog</Box>
+      </Box>
     </div>
   );
 }
