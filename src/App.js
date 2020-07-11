3bd198a989d2bb1b649e11bb202f8b139bf9010e
Kanata <thunderpooh3@gmail.com>
2020-07-11 20:31:53 +0900

    Clean everything

--- src/App.js
+++ src/App.js
@@ -1,45 +1,9 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import './App.css';
-
-const useStyles = makeStyles((theme) => ({
-  appBar: {
-    background: "#fff"
-  },
-  hero: {
-    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
-    url('https://images.unsplash.com/photo-1542324253097-09d465f007bc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80')`,
-    height: "500px",
-    backgroundPosition: "center",
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "cover",
-    position: "relative",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    color: "#fff",
-    fontSize: "4rem"
-  }
-}));
 
 function App() {
-  const classes = useStyles();
   return (
     <div className="App">
-      <AppBar className={classes.appBar} position="static">
-        <Toolbar>
-          <Typography variant="h6" color="primary">
-            Blog
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Box className={classes.hero}>
-        <Box>React Blog</Box>
-      </Box>
+    
     </div>
   );
 }
