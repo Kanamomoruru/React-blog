6291176b19b02df9ead1b13bf32c21d31b3e24b6
Kanata <thunderpooh3@gmail.com>
2020-07-11 21:37:13 +0900

    add router and css

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const naviLinks = [
     {
@@ -26,7 +27,7 @@ export default function Navigation() {
             <ul>
                 {naviLinks.map((link, index) => (
                     <li key={index}>
-                        {link.title}
+                        <Link to={link.path}>{link.title}</Link>
                     </li>
                 ))}
             </ul>
