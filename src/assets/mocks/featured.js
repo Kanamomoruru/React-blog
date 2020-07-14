608081c66353a220828324e6e3f2ac84fe563691
Kanata <thunderpooh3@gmail.com>
2020-07-14 21:29:33 +0900

    add mocks

---
+++ src/assets/mocks/featured.js
@@ -0,0 +1,32 @@
+import moment from 'moment';
+
+export default [
+    {
+        title: 'Can anyone code?',
+        data: moment().format('MMMM DD, YYYY'),
+        categories: ['Tech Culture', 'Tech News'],
+        link: '#',
+        image: 'anyone_can_code.jpg',
+    },
+    {
+        title: 'Using AWS S3 for Storing Blog Images',
+        data: moment().format('MMMM DD, YYYY'),
+        categories: ['Cloud'],
+        link: '#',
+        image: 'cloud_storage.jpeg',
+    },
+    {
+        title: 'Popular Programming Languages in 2020',
+        data: moment().format('MMMM DD, YYYY'),
+        categories: ['Tech News', 'Tech Culture'],
+        link: '#',
+        image: 'programming_language.jpeg',
+    },
+    {
+        title: 'Braing Hacks for Learning to Programming',
+        data: moment().format('MMMM DD, YYYY'),
+        categories: ['Brain Health'],
+        link: '#',
+        image: 'neuron.jpg',
+    }
+]
---
+++ src/assets/mocks/trending.js
@@ -0,0 +1,32 @@
+import moment from 'moment';
+
+export default [
+    {
+        title: 'Software Engineer Salary in 2020',
+        data: moment().format('MMMM DD, YYYY'),
+        categories: ['Tech Culture'],
+        link: '#',
+        image: 'money.jpg',
+    },
+    {
+        title: 'GraphQL vs REST',
+        data: moment().format('MMMM DD, YYYY'),
+        categories: ['React', 'JavaScript'],
+        link: '#',
+        image: 'apollo_glaphql.png',
+    },
+    {
+        title: 'A Day in the Life of a Programmer',
+        data: moment().format('MMMM DD, YYYY'),
+        categories: ['Tech Culture'],
+        link: '#',
+        image: 'puzzled_programmer.jpeg',
+    },
+    {
+        title: 'Braing Hacks for Learning to Program',
+        data: moment().format('MMMM DD, YYYY'),
+        categories: ['Brain Health'],
+        link: '#',
+        image: 'neuron.jpg',
+    }
+]
