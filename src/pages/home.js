35e1b7f7a94ef52475713c6113ec1fc62ada8645
Kanata <thunderpooh3@gmail.com>
2020-07-15 21:10:12 +0900

    add post masonry file

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import {PostMasonry} from '../components/common/post-masonry'
+import trending from '../assets/mocks/trending'
 
 export default function Home() {
-    return <section className="container home"></section>
+    return (
+        <section className="container home">
+            <div className="row">
+                <h2>Trending Posts</h2>
+                <PostMasonry posts={trending} column={3}/>
+            </div>
+        </section>
+    )
 }
