34d3b4320528200066cde25f28345c4faafc3860
Kanata <thunderpooh3@gmail.com>
2020-07-16 20:33:07 +0900

    created three files in common

---
+++ src/components/common/post-masonry.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import MasonryPost from './masonry-post'
+
+export default function PostMasonry({ posts, columns, tagOnTop}) {
+    return (
+        <section className="masonly" style={{gridTemplateColumns: `repeat(${columns}), minmax(275px, 1fr)`}}>
+            {posts.map((post, index) =>
+                <MasonryPost {...{post, index, tagOnTop, key: index}} />
+            )}
+        </section>
+    )
+}
