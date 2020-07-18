f484cc2752cfc73e10ec8def77da3744bc220830
Kanata <thunderpooh3@gmail.com>
2020-07-18 20:43:34 +0900

    add styles file

--- src/components/common/masonry-post.js
+++ src/components/common/masonry-post.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import categoryColors from './styles'
 
 export default function MasonryPost({ post, tagOnTop}) {
     const style = {backgroundImage: `url("${require(`../../assets/images/${post.image}`)}")`} ;
@@ -6,7 +7,12 @@ export default function MasonryPost({ post, tagOnTop}) {
         <a className="masonry-post overlay" style={style} href={post.link}>
             <div className="image-text">
                 <div className="tags-container">
-
+                    {
+                        post.categories.map((tag, index) => 
+                            <span key={index} className="tag" style={{backgroundColor: categoryColors[tag]}}>
+                                {tag.toUpperCase()}
+                            </span>
+                    )}
                 </div>
                 <div>
                     <h2 className="image-title">{post.title}</h2>
