26dc13ef1c299a38e7461b9dd8b6e686e95e8c9b
Kanata <thunderpooh3@gmail.com>
2020-08-03 21:25:32 +0900

    add tag-row files

--- src/components/common/post-grid.js
+++ src/components/common/post-grid.js
@@ -5,10 +5,28 @@ import {Pagination} from 'antd'
 export default function PostGrid ({posts}) {
     const [pageSize, setPageSize] = useState(9)
     const [current, setCurrent] = useState(1)
+
+    const paginatedPosts = useMemo(() => {
+        const lastIndex = current * pageSize
+        const firstIndex = lastIndex - pageSize
+
+        return posts.slice(firstIndex, lastIndex)
+    }, [current, pageSize]) 
+
+    
     return (
         <section className="grid-pagination-container">
             <section className="post-grid container">
-
+                {paginatedPosts.map((post, index) => {
+                    // eslint-disable-next-line no-unused-expressions
+                    <div className="post-container">
+                        <figure>
+                            <Link to={post.Link}>
+                                <img src={require(`../../assets/images/${post.image}`)} alt={post.image}/>
+                            </Link>
+                        </figure>
+                    </div>
+                })}
             </section>
             <Pagination 
                 simple 
